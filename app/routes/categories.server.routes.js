10a9112cf0cad7d9fd42416234b96cfbece680dc
Justin Beuck <jbeuck@yahoo.com>
2015-12-08 23:45:01 -0600

    adjusted route to match tutorial will change for subaru parts db

--- app/routes/categories.server.routes.js
+++ app/routes/categories.server.routes.js
@@ -1,6 +1,9 @@
 'use strict';
 
 module.exports = function(app) {
-	// Routing logic   
-	// ...
+    var categories = require('../../app/controllers/categories.server.controller');
+
+    app.route('/categories')
+      .get(categories.list)
+      .post(categories.create);
 };
